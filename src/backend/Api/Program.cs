954686f59d60d248dc8df5ac7162f57f16291429
Thomas Pettersen / Yrki <thomas@yrki.no>
2026-03-14 11:43:05 +0100

    Added login.

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -1,46 +1,85 @@
+using System.Text;
+using Api.Configuration;
+using Api.Services;
 using Core.Contexts;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<DatabaseContext>(options => 
-                                            options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnectionString")));
+builder.Services.Configure<CorsOptions>(builder.Configuration.GetSection("Cors"));
+builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection("Email"));
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+builder.Services.Configure<MagicLinkOptions>(builder.Configuration.GetSection("MagicLink"));
 
-// Add cors policy for localhost
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnectionString")));
+
+builder.Services.AddScoped<ITokenHasher, Sha256TokenHasher>();
+builder.Services.AddScoped<IEmailSender, SmtpEmailSender>();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IMagicLinkService, MagicLinkService>();
+
+var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>() ?? new JwtOptions();
+var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtOptions.Issuer,
+            ValidAudience = jwtOptions.Audience,
+            IssuerSigningKey = signingKey,
+            ClockSkew = TimeSpan.FromMinutes(1)
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy",
-               builder => builder
-                          .AllowAnyMethod()
-                          .AllowAnyHeader()
-                          .AllowAnyOrigin());
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        var corsOptions = builder.Configuration.GetSection("Cors").Get<CorsOptions>() ?? new CorsOptions();
+        if (corsOptions.AllowedOrigins.Length == 0)
+        {
+            throw new InvalidOperationException("Cors:AllowedOrigins must contain at least one origin.");
+        }
+
+        policy.WithOrigins(corsOptions.AllowedOrigins)
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
 });
 
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var databaseContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    databaseContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
-// app.UseHttpsRedirection();
-
 app.UseCors("CorsPolicy");
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
